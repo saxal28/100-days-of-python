08970e312f52cf3fda69b1c9ca19304565d7fc53
Alan Sax <saxal28@gmail.com>
2023-11-08 09:18:07 -0600

    day1

---
+++ day1/index.py
@@ -0,0 +1,9 @@
+# Day 1
+# create a band name generator using input and print
+
+print("Welcome to the Band Name Generator")
+print("What's the city that you grew up in?")
+city = input()
+print("What's your pet's name?")
+pet_name = input()
+print("Your band name could be " + city + " " + pet_name)
